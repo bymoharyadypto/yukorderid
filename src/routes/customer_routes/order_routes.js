ec22b23f6128b67591e5a1fd83792dae72884942
bymoharyadypto <bymoharyadypto@gmail.com>
2025-06-28 10:52:26 +0700

    fix

--- src/routes/customer_routes/order_routes.js
+++ src/routes/customer_routes/order_routes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const OrderController = require("../../controllers/customer_controllers/order_controller");
-
 // router.post("/preview", OrderController.previewOrder);
 // router.post("/", OrderController.createOrder);
 router.get("/", OrderController.getCustomerOrders);
-
+router.put("/:orderId", OrderController.confirmOrderDelivered);
 
 module.exports = router;
